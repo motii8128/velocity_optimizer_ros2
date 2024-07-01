3b2846f8565e34da001d362a3f6eda0fdfee229b
motii8128 <default270robo@gmail.com>
2024-07-01 10:46:19 +0900

    Update

--- include/velocity_optimizer_ros2/vel_opt_ros2.hpp
+++ include/velocity_optimizer_ros2/vel_opt_ros2.hpp
@@ -1,4 +1,18 @@
 #ifndef VEL_OPT_ROS2_NODE_HPP_
 #define VEL_OPT_ROS2_NODE_HPP_
 
+#include <rclcpp/rclcpp.hpp>
+
+namespace vel_opt_ros2
+{
+    class VelocityOptimizerROS2 : public rclcpp::Node
+    {
+        public:
+        explicit VelocityOptimizerROS2(rclcpp::NodeOptions options= rclcpp::NodeOptions());
+
+        private:
+        void topic_callback();
+    };
+}
+
 #endif
